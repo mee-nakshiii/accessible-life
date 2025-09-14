2f888090b9d05721c4f05e6b7b40df276c90d4bc
Harsha Esmayil <harshaesmayil13@gmail.com>
2025-09-14 22:31:37 +0530

    Integration and testrun 1 - Harsha

---
+++ src/main/java/com/accessiblelife/ratingreview/RatingReview.java
@@ -0,0 +1,36 @@
+package com.accessiblelife.ratingreview;
+
+public class RatingReview
+{
+    private String user;
+    private int rating;
+    private String review;
+
+    public RatingReview(String user, int rating, String review)
+    {
+        this.user = user;
+        this.rating = rating;
+        this.review = review;
+    }
+
+    public String getUser()
+    {
+        return user;
+    }
+
+    public int getRating()
+    {
+        return rating;
+    }
+
+    public String getReview()
+    {
+        return review;
+    }
+
+    @Override
+    public String toString()
+    {
+        return user + " rated " + rating + "/5 - " + review;
+    }
+}
