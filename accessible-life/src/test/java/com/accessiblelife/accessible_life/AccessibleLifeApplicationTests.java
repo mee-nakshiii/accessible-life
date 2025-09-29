ef8b9867515a72bf87e0f9e9158140dacba37c41
Elaine Maria Binu <elainemariabinu@gmail.com>
2025-09-29 21:30:27 +0530

    added Admin class with constructors and methods/changes

---
+++ accessible-life/src/test/java/com/accessiblelife/accessible_life/AccessibleLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accessiblelife.accessible_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibleLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
